8ba7df3221b490595d05316823bc65d3dc7569e3
liuke <18162327131@163.com>
2019-12-18 20:57:55 +0800

    init

---
+++ src/main/java/com/wishhust/leetcode/SearchMatrix.java
@@ -0,0 +1,7 @@
+package com.wishhust.leetcode;
+
+/**
+ * Created by liuke on 2019/12/18 20:50
+ */
+public class SearchMatrix {
+}
---
+++ src/main/java/com/wishhust/test/Test.java
@@ -0,0 +1,10 @@
+package com.wishhust.test;
+
+/**
+ * Created by liuke on 2019/12/18 16:08
+ */
+public class Test {
+  public static void main(String[] args) {
+    System.out.println("中文");
+  }
+}
