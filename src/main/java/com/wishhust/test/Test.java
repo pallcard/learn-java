1d3fdc8d49ef6f8b90838ed15f28972283693447
liuke <18162327131@163.com>
2020-03-25 23:29:42 +0800

    添加jdk8

--- src/main/java/com/wishhust/test/Test.java
+++ src/main/java/com/wishhust/test/Test.java
@@ -1,11 +1,18 @@
 package com.wishhust.test;
 
+import java.util.HashMap;
+import java.util.Map;
+
 /**
  * Created by liuke on 2019/12/18 16:08
  */
 public class Test {
   public static void main(String[] args) {
     System.out.println("中文");
+
+
+    Map<String, Double> map = new HashMap<>();
+    map.put("k1", 1.0);
 
     String a = "￥";
     System.out.println(a.length());
